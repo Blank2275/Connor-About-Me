e6e59931f4af7217d9da7ce0b961bbf647556290
Connor <cfr3.14156@gmail.com>
2021-10-05 10:56:47 -0600

    Initial Commit

---
+++ Connor-About-Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Connor-About-Me
+//
+//  Created by Connor Reed on 10/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
